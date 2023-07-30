2d10d99d10c154f18005a3becec999d3d8c55632
Anand Raj Singh <singhpariharanandraj@gmail.com>
2023-07-30 14:18:04 +0530

    MainLayout

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useMemo} from 'react'
+import React, { useState, useMemo } from 'react'
 import styled from "styled-components";
 import { useGlobalContext } from './context/globalContext';
 
@@ -9,30 +9,30 @@ function App() {
   console.log(global)
 
   const orbMemo = useMemo(() => {
-    return 
-  },[])
+    return <Orb />
+  }, [])
 
   const AppStyled = styled.div`
     height: 100vh;
-  background-image: url(${props => props.bg});
-  position: relative;
-  main{
-    flex: 1;
-    background: rgba(252, 246, 249, 0.78);
-    border: 3px solid #FFFFFF;
-    backdrop-filter: blur(4.5px);
-    border-radius: 32px;
-    overflow-x: hidden;
-    &::-webkit-scrollbar{
-      width: 0;
+    background-image: url(${props => props.bg});
+    position: relative;
+    main{
+      flex: 1;
+      background: rgba(252, 246, 249, 0.78);
+      border: 3px solid #FFFFFF;
+      backdrop-filter: blur(4.5px);
+      border-radius: 32px;
+      overflow-x: hidden;
+      &::-webkit-scrollbar{
+        width: 0;
+      }
     }
-  }
 `;
 
   return (
-    <div className="App">
-      
-    </div>
+    <AppStyled bg={bg} className="App">
+      {orbMemo}
+    </AppStyled>
   );
 }
 
